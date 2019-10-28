074cc6ad913d38b47ab618a168828c01cad0dbb3
VVishnevskii <50050949+VVishnevskii@users.noreply.github.com>
2019-10-28 18:03:57 +0300

    Доработка слайдера

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ var photos = document.querySelectorAll('.photos img');
 var i = 0;
 
 buttonNext.onclick = function () {
-  photos[i].style.display = 'none';
+  photos[i].classList.remove('showed');
   i++;
   
   if (i >= photos.length) {
     i = 0;
   }
 
-  photos[i].style.display = 'block';
+  photos[i].classList.add('showed');
 }
 
 buttonPrev.onclick = function () {
-  photos[i].style.display = 'none';
+  photos[i].classList.remove('showed');
   i--;
   
   if (i < 0) {
     i = photos.length - 1;
   }
 
-  photos[i].style.display = 'block';
+  photos[i].classList.add('showed');
 }
