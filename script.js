7963205a23ec1826f665ed1ac7affd39babea374
VVishnevskii <50050949+VVishnevskii@users.noreply.github.com>
2019-11-14 10:17:39 +0300

    Настройка слайдера

--- script.js
+++ script.js
@@ -3,7 +3,29 @@ var buttonPrev = document.querySelector('.prev');
 var photos = document.querySelectorAll('.photos img');
 var i = 0;
 
-buttonNext.onclick = function () {
+buttonNext.addEventListener('click', function () {
+  photos[i].classList.remove('showed');
+  i++;
+  
+  if (i >= photos.length) {
+    i = 0;
+  }
+
+  photos[i].classList.add('showed');
+})
+
+buttonPrev.addEventListener('click', function () {
+  photos[i].classList.remove('showed');
+  i--;
+  
+  if (i < 0) {
+    i = photos.length - 1;
+  }
+
+  photos[i].classList.add('showed');
+})
+
+var forInterval = function () {
   photos[i].classList.remove('showed');
   i++;
   
@@ -14,13 +36,13 @@ buttonNext.onclick = function () {
   photos[i].classList.add('showed');
 }
 
-buttonPrev.onclick = function () {
-  photos[i].classList.remove('showed');
-  i--;
-  
-  if (i < 0) {
-    i = photos.length - 1;
-  }
+var sliderInterval = setInterval(forInterval, 3500);
 
-  photos[i].classList.add('showed');
-}
+
+
+buttonNext.addEventListener('click', function () {
+  clearInterval(sliderInterval);
+})
+buttonPrev.addEventListener('click', function () {
+  clearInterval(sliderInterval);
+})
